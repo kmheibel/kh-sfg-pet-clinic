e155bc112100e4810cb45f00c7f63d046d2881aa
Kristin Heibel <kmheibel@gmail.com>
2021-10-06 10:26:47 -0500

    initial commit as multi-module

---
+++ pet-clinic-web/src/test/java/me/kmheibel/khsfgpetclinic/KhSfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kmheibel.khsfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhSfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/me/kmheibel/khsfgpetclinic/KhSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package me.kmheibel.khsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhSfgPetClinicApplication.class, args);
+    }
+
+}
